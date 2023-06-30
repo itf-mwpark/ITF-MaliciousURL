f6fb31574f4cb0be637d2bf3403229953a110974
itf-mwpark <mw.park@itforest.net>
2023-06-30 11:26:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, request, send_file
 import requests
-import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     path = 'files/'
-    # remote_addr = request.remote_addr
-    remote_addr = '172.21.0.1'
+    remote_addr = request.remote_addr
     api_param = {"key": "me58wbfwetp7mmq7is1fkqcgxcot", "host": remote_addr}
     
     res = requests.get(url="https://api.itforest.net/ipinfo", params=api_param)
+    print(f'[*] api response: {res.json()}')
+    
     country_code = res.json()["data"].get("country_code", None)
 
     if country_code == "KR":
