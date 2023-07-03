ac7d19fe663b0587de735a04c936401c9618f63b
itf-mwpark <mw.park@itforest.net>
2023-07-03 14:05:04 +0900

    Update: whois api call and file path logic

--- app.py
+++ app.py
@@ -1,30 +1,49 @@
 from flask import Flask, request, send_file
 import requests
 import os
+import json
 
 app = Flask(__name__)
+
+malicious_file = os.environ.get("MALICIOUS_FILE_PATH", "./files/mimikatz.txt")
+none_malicious_file = os.environ.get("NONE_MALICIOUS_FILE_PATH", "./files/none_malicious_file.txt")
 
 @app.route('/')
 def home():
     remote_addr = request.remote_addr
-    api_param = {"key": "me58wbfwetp7mmq7is1fkqcgxcot", "host": remote_addr}
     
-    res = requests.get(url="https://api.itforest.net/ipinfo", params=api_param)
-    
-    country_code = res.json()["data"].get("country_code", None)
-    print(f'[*] remote_addr: {remote_addr}, country: {country_code}')
+    try:
+        api_param = {
+            "serviceKey": "WV8lzb2Hk0HQZV08PpT2CxFSCGm8Nz5cZURni1mvK+zd/72mRL9b68qKw7CmJxVCHM1QOrp1uR84YL8JuB4O+g==",
+            "query": remote_addr,
+            "answer": "json",
+        }
 
-    if country_code == "KR":
-        path = malicious_file
+        res = requests.get(url="http://apis.data.go.kr/B551505/whois/ipas_country_code", params=api_param, verify=False).json()
 
-    else:
+        response = res.get("response", {})
+        whois = response.get("whois", {})
+        country_code = whois.get("countryCode", None)
+        
+        print(f'[*] remote_addr: {remote_addr}, country: {country_code}')
+
+        if not country_code or country_code == "none" or country_code != "KR":
+            path = none_malicious_file
+
+        else:
+            path = malicious_file
+
+    except json.JSONDecodeError:
         path = none_malicious_file
-
+        print("Invalid JSON format.")
+    
+    except KeyError:
+        path = none_malicious_file
+        print("Key not found.")
+    
     return send_file(path, as_attachment=True)
 
 if __name__ == '__main__':
-    malicious_file = os.environ.get("MALICIOUS_FILE_PATH", "./files/mimikatz.txt")
-    none_malicious_file = os.environ.get("NONE_MALICIOUS_FILE_PATH", "./files/none_maliicious_file.txt")
 
     print(f"[*] Malicious file is --> {malicious_file}")
     print(f"[*] None malicious file is --> {none_malicious_file}")
